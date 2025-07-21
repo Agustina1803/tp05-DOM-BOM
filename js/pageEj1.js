cbd944f9884b87f325577c8adb0bb24bc4076134
AgusBulacio <agusbulacio1803@gmail.com>
2025-07-21 19:25:45 -0300

    agrege la logica para que me muestre un mensaje de que el juego comenzo,para que me valide que el numero este entre 1 y 10

--- js/pageEj1.js
+++ js/pageEj1.js
@@ -1,23 +1,41 @@
 let numeroAleatorio = 0;
 let numerosIngresados = [];
+let juegoActivo = false;
 
 
 function comenzarJuego(){
     numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+    juegoActivo = true;
+    const parrafo = document.getElementById("p-juego");
+    parrafo.classList.remove("d-none");
 }
+
 
 function recibirNumero(e){
     e.preventDefault();
+    if( juegoActivo === false){
+        alert("Primero presiona 'Comenzar juego'");
+        formulario.reset();
+         return;
+    }
     const input = document.querySelector('.form-control');
      numeroRecibido = parseInt (input.value);
      numerosIngresados.push(numeroRecibido);
-     mostrarResultado(numeroAleatorio, numeroRecibido);
-     formulario.reset();     
+       if (numeroRecibido < 1 || numeroRecibido > 10) {
+        alert("Por favor, ingresa un número entre 1 y 10.");
+        formulario.reset();
+        return;
+    }
+     numerosIngresados.push(numeroRecibido);
+     mostrarResultado(numeroAleatorio, numeroRecibido);     
+     formulario.reset();
 }
 
 function mostrarResultado(numeroAleatorio, numeroRecibido){
     if(numeroAleatorio === numeroRecibido){
-        alert(`Adivinaste el numero! ${numeroRecibido}`);
+        alert(`Adivinaste el numero! es el:  ${numeroRecibido}, presiona Comenzar juego! para volver a jugar'`);
+        juegoActivo = false;
+        document.getElementById("p-juego").classList.add("d-none");
     }else if(numeroAleatorio > numeroRecibido){
         alert(`El numero ingresado ${numeroRecibido} es menor que el numero generado`);
     }else{
